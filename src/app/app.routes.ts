56c95935a743c4c789f98c51768f8f63ec99c3cb
Franco Bernardele <francobernar@gmail.com>
2023-12-09 16:53:15 -0300

    commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent} from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'nav-bar', component: NavBarComponent},
+];
+
+
+
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavBarComponent } from '../../nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    NavBarComponent,
+    RouterModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
