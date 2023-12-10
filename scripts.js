a25d1313fa0811c6241fcccd068cc24b2569262f
Franco Bernardele <francobernar@gmail.com>
2023-12-10 14:57:16 -0300

    pokeapi fetch

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 var slideIndex = 1;
-document.addEventListener('DOMContentLoaded', function() {
+let imagen
+
+document.addEventListener('DOMContentLoaded', async function() {
+  let pokeimg = document.getElementById('pokeimagen');
 
   showDivs(slideIndex);
+  
+  pokeimg.src = await returnPokeImg(1, 151);
 });
 
 function plusDivs(n) {
@@ -11,12 +16,23 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("proyecto");
-    console.log('slideIndex:', n); // add this line
-    console.log('number of divs:', x.length); // add this line
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block";
+}
+
+function randomNum(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  
+}
+
+async function returnPokeImg()
+{
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + randomNum(1, 151));
+  let respuestaimagen = await response.json();
+  imagen = respuestaimagen.sprites.front_default;
+  return imagen;
 }
